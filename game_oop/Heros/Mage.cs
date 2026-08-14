56895e7f18f1b3988653b71ed7bd6137e7c8eaca
melyga <melyga811@gmail.com>
2026-08-14 21:29:03 +0500

    работающая ветвь игры

--- game_oop/Heros/Mage.cs
+++ game_oop/Heros/Mage.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Game
+﻿namespace Game
 {
     public class Mage : Hero
     {
--- game_oop/Heros/Paladin.cs
+++ game_oop/Heros/Paladin.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Game
+﻿namespace Game
 {
     public class Paladin : Hero
     {
